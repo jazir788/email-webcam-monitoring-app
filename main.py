f02ace94a1473a5069e965536cb00f7cb5a6f679
jazir788 <jazirsx5@gmail.com>
2023-11-06 11:53:26 +0000

    Created images when object is detected

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import glob
+
 import cv2
 import time
 from emailing import send_email
@@ -6,9 +8,11 @@ time.sleep(1)
 
 first_frame = None
 status_list = []
+count=1
 while True:
     status = 0
     check, frame = video.read()
+
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21,21), 0)
 
@@ -31,12 +35,17 @@ while True:
         rectangle = cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count += 1
+            all_images = glob.glob("images/*.png")
+            index  = int(len(all_images)/2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(image_with_object)
 
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
